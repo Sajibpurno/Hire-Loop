4b0b7262c101998e823614cb5d53c43f00555384
Sajibpurno <chasajib86@gmail.com>
2026-06-10 16:29:18 +0600

    admin pannel job create job showing done

---
+++ src/app/deshboard/recruiter/company/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompanyPage = () => {
+    return (
+        <div>
+            <h2>Company Profile</h2>
+        </div>
+    );
+};
+
+export default CompanyPage;
--- src/components/dashboard/DashboardSidebar.jsx
+++ src/components/dashboard/DashboardSidebar.jsx
@@ -1,27 +1,30 @@
 
 import {Bell, Envelope, Gear, House, LayoutSideContentLeft, Magnifier, Person} from "@gravity-ui/icons";
 import {Button, Drawer} from "@heroui/react";
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
 
 const DeshboardSidebar = () => {
      const navItems = [
-    {icon: House, label: "Home"},
-    {icon: Magnifier, label: "Search"},
-    {icon: Bell, label: "Notifications"},
-    {icon: Envelope, label: "Messages"},
-    {icon: Person, label: "Profile"},
-    {icon: Gear, label: "Settings"},
+    {icon: House, href:'/deshboard/recruiter', label: "Home"},
+    {icon: Magnifier, href:'/deshboard/recruiter/jobs', label: "Jobs"},
+    {icon: Bell, href:'/deshboard/recruiter/jobs/new', label: "Create A Job"},
+    {icon: Briefcase, href:'/deshboard/recruiter/company', label: "Company Profile"},
+    {icon: Envelope,href:'/deshboard/recruiter/message', label: "Messages"},
+    {icon: Person, href:'/deshboard/recruiter/profile', label: "Profile"},
+    {icon: Gear, href:'/deshboard/recruiter/Setting', label: "Settings"},
   ];
 
   const navContent = <nav className="flex flex-col gap-1">
                 {navItems.map((item) => (
-                  <button
+                  <Link
                     key={item.label}
                     className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
-                    type="button"
+                    href={item.href}
                   >
                     <item.icon className="size-5 text-muted" />
                     {item.label}
-                  </button>
+                  </Link>
                 ))}
               </nav>
     return (
